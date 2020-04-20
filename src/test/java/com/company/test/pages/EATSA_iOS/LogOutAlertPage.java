e31607dd27533b95f528d0a53440c93861feb068
Nicolas Silvestre <nicolas.silvestre1@gmail.com>
2020-04-20 11:13:09 -0300

    uploading final project

---
+++ src/test/java/com/company/test/pages/EATSA_iOS/LogOutAlertPage.java
@@ -0,0 +1,28 @@
+package com.company.test.pages.EATSA_iOS;
+
+import com.company.framework.pageobject.MobileBasePO;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import sun.rmi.runtime.Log;
+
+public class LogOutAlertPage extends MobileBasePO {
+
+    String cancelId = "Cancel";
+    String logOutId = "Log out";
+
+    public LogOutAlertPage(AppiumDriver driver){
+        super(driver);
+    }
+
+
+    public void tapCancel() {
+        getWaitDriver().until(ExpectedConditions.elementToBeClickable(By.id(cancelId))).click();
+    }
+
+
+    public void tapLogOut() {
+        getWaitDriver().until(ExpectedConditions.elementToBeClickable(By.id(logOutId))).click();
+    }
+}
