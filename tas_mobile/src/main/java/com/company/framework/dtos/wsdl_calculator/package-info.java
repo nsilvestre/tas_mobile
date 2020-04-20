41b9a7f578d22e1102a9e2cc1ac972a1a5a86252
Nicolas Silvestre <nicolas.silvestre1@gmail.com>
2020-04-20 11:11:26 -0300

    wrong project uploaded

--- tas_mobile/src/main/java/com/company/framework/dtos/wsdl_calculator/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://tempuri.org/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.company.framework.dtos.wsdl_calculator;
--- tas_mobile/src/test/resources/datasets/getProducts.sql
+++
@@ -1,3 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `getProducts`()
-BEGIN INSERT INTO `test`.product (id, name, price, stock, length, width, height)
-VALUES (2, "radio", 30, 200, 10,20, 18); END
